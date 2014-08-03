10a5045de1edce1959cbaa270b616879808dee7c
Chad Warren <warren@bluehiveinteractive.com>
2014-08-03 14:23:27 -0400

    Layout Updates

--- inc/template-layouts.php
+++ inc/template-layouts.php
@@ -7,12 +7,12 @@ function bluehive_layouts() {
 	if (!is_admin()) {
 		// Sidebar Content
 		if ( is_page_template('page-sidebarContent.php')) {
-			wp_register_style('sidebar-content', PARENT_URL.'/assets/build/layout/sidebar-content.css', false, '1.0.0', 'all');
+			wp_register_style('sidebar-content', PARENT_URL.'/assets/css/build/layout/sidebar-content.css', false, '1.0.0', 'all');
 			wp_enqueue_style('sidebar-content');
 		}
 		//Content Sidebar
 		if ( is_page_template('page-contentSidebar.php')) {
-			wp_register_style('content-sidebar', PARENT_URL.'/assets/build/layout/content-sidebar.css', false, '1.0.0', 'all');
+			wp_register_style('content-sidebar', PARENT_URL.'/assets/css/build/layout/content-sidebar.css', false, '1.0.0', 'all');
 			wp_enqueue_style('content-sidebar');
 		}
 	}
