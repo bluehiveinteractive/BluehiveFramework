c4182a60a2214fb04647659a2fd92fb122540a48
Chad Warren <warren@bluehiveinteractive.com>
2014-08-03 13:46:34 -0400

    New layout
    Addition of content -> sidebar

--- inc/template-layouts.php
+++ inc/template-layouts.php
@@ -10,6 +10,10 @@ function bluehive_layouts() {
 			wp_register_style('sidebar-content', PARENT_URL.'/assets/build/layout/sidebar-content.css', array(), '1.0.0');
 			wp_enqueue_script('sidebar-content');
 		}
+		if ( is_page_template('page_contentSidebar.php')) {
+			wp_register_style('content-sidebar', PARENT_URL.'/assets/build/layout/content-sidebar.css', array(), '1.0.0');
+			wp_enqueue_script('content-sidebar');
+		}
 	}
 }
 add_action('wp_enqueue_style', 'bluehive_layouts');
